ccae6de5590b1a86523292f27e05775e38782215
vincentjedi <kitivincent@gmail.com>
2024-08-16 13:25:05 +0300

    second commit

--- migrate.py
+++ migrate.py
@@ -1,32 +1,35 @@
 import pymysql
 import psycopg2
+from dotenv import load_dotenv
+import os
 
-# Establishing MySQL connection
+# Load environment variables from .env file
+load_dotenv()
+
+# MySQL connection using environment variables
 mysql_conn = pymysql.connect(
-    host="localhost",
-    user="root",
-    passwd="vince123",
-    database="magari"
+    host=os.getenv("MYSQL_HOST"),
+    user=os.getenv("MYSQL_USER"),
+    passwd=os.getenv("MYSQL_PASSWORD"),
+    database=os.getenv("MYSQL_DATABASE")
 )
 
-# Establishing PostgreSQL connection
+# PostgreSQL connection using environment variables
 post_conn = psycopg2.connect(
-    host="localhost",
-    user="postgres",
-    password="vince123",
-    dbname="cars"
+    host=os.getenv("POSTGRES_HOST"),
+    user=os.getenv("POSTGRES_USER"),
+    password=os.getenv("POSTGRES_PASSWORD"),
+    dbname=os.getenv("POSTGRES_DATABASE")
 )
 
-# Fetching the data from MySQL database
+# Rest of your code remains the same
 mysql_cursor = mysql_conn.cursor()
 mysql_cursor.execute("SELECT * FROM tools")
 data = mysql_cursor.fetchall()
 columns = [desc[0] for desc in mysql_cursor.description]
 
-# Creating the same table in PostgreSQL
 post_cursor = post_conn.cursor()
 
-# Drop the table if it exists
 drop_table_query = "DROP TABLE IF EXISTS tools_table"
 post_cursor.execute(drop_table_query)
 post_conn.commit()
@@ -42,13 +45,11 @@ CREATE TABLE tools_table (
 post_cursor.execute(create_table_query)
 post_conn.commit()
 
-# Inserting the data to the PostgreSQL table
 placeholders = ', '.join(['%s'] * len(columns))
 insert_query = f"INSERT INTO tools_table ({', '.join(columns)}) VALUES ({placeholders})"
 post_cursor.executemany(insert_query, data)
 post_conn.commit()
 
-# Closing all the connections
 mysql_cursor.close()
 mysql_conn.close()
 post_cursor.close()
